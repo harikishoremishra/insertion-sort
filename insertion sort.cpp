a5390a1151fdd4a32e889a9b33a8d9529ff3dbd3
harikishoremishra <122509569+harikishoremishra@users.noreply.github.com>
2023-07-14 10:45:42 +0530

    Add files via upload

---
+++ insertion sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<" input an array of element which you have to sort let us say : 6 elements";
+	int i;
+	int a[6];
+	for(i=0;i<6;i++) cin>>a[i];
+	
+	int key;
+	int j;
+	
+	for (j=1;j<6;j++)
+	{
+		key =a[j];
+	i=j-1;
+	
+	while(a[i]>key && i>0)
+	{
+		a[i+1]=a[i];
+		i--;
+	}
+	a[i+1]=key;
+	}
+		for(i=0;i<6;i++) cout<<a[i]<<" ";
+	    
+}
